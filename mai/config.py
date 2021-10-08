d72d0e7c7f4ce24e09fbd914749d1b267f1667f0
Ping Wu <wpwupingwp@outlook.com>
2021-10-08 17:17:36 +0800

    use local js

--- mai/config.py
+++ mai/config.py
@@ -2,15 +2,19 @@
 
 from mai import root
 
-
+# database
 SQLALCHEMY_DATABASE_URI = 'sqlite:///mai.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+# upload
 # max filesize 10mb
 MAX_CONTENT_LENGTH = 10 * 1024 * 1024
 CSRF_ENABLED = True
-SECRET_KEY = '2021'
 UPLOAD_FOLDER = root / 'upload'
 UPLOADED_PHOTOS_DEST = UPLOAD_FOLDER / 'img'
 for i in UPLOAD_FOLDER, UPLOADED_PHOTOS_DEST:
     if not i.exists():
         i.mkdir()
+# safe
+SECRET_KEY = '2021'
+# bootstrap
+BOOTSTRAP_SERVE_LOCAL = True

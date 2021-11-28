1d0a7bde154efb9118ef395681cf920700b78bcb
Ping Wu <wpwupingwp@outlook.com>
2021-11-28 17:24:22 +0800

    use `flask db revision --rev-id` to try to fix migrate error

--- run.bat
+++ run.bat
@@ -1,6 +1,7 @@
 set FLASK_APP=mai/__init__.py
 rem flask db init
-rem flask db migrate
+rem flask db revision --rev-id xxxxxxxx
+flask db migrate
 flask db upgrade
 python add_data.py
 flask run -h 0.0.0.0 -p 2021
